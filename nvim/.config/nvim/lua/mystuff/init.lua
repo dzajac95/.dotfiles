076aa98ba99d0d87d979fa878dc122b985e45905
dzajac <dzajac95@gmail.com>
2023-12-27 02:18:15 -0500

    migrate to lazy.nvim

--- nvim/.config/nvim/lua/mystuff/init.lua
+++ nvim/.config/nvim/lua/mystuff/init.lua
@@ -2,3 +2,19 @@ require("mystuff.remap")
 require("mystuff.set")
 require("mystuff.func")
 require("mystuff.filetype")
+
+-- Bootstrap lazy vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("mystuff.lazy")
---
+++ nvim/.config/nvim/lua/mystuff/lazy.lua
@@ -0,0 +1,28 @@
+require("lazy").setup({
+    {
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {'Mofiqul/vscode.nvim'},
+    {'tpope/vim-repeat'},
+    {'tpope/vim-fugitive'},
+    {'tpope/vim-commentary'},
+    {'tpope/vim-surround'},
+    {'tpope/vim-abolish'},
+    {'junegunn/vim-easy-align'},
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
+    },
+    {'williamboman/mason.nvim',
+        init = function()
+            require("mason").setup()
+        end},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+}
+)
